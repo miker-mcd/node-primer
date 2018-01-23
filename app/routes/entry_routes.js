d8ae233cafdcf041e2c8059d5766d6f4624bce8a
miker-mcd <michaelrmcd@gmail.com>
2018-01-22 18:41:31 -0800

    Add /entries/:id route to read an entry

--- app/routes/entry_routes.js
+++ app/routes/entry_routes.js
@@ -1,16 +1,43 @@
 // Create a wrapper function that takes an Express instance and a database as arguments
 
-module.exports = function (app, db) {
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function (app, myDB) {
 
   // CREATE an entry
   app.post('/entries', function (req, res) {
 
     // Test the POST request to localhost:8000/entries using Postman
-    res.send('Post working!');
+    // res.send('Post working!');
 
     // Check request by logging the req.body to the server
     console.log(req.body); // { title: 'First Entry', body: 'This is my first entry!' }
+
+    // Call insert on entries collection to create new entry
+    const entry = { text: req.body.body, title: req.body.title };
+    myDB.collection('entries').insert(entry, function (err, result) {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   })
+
+  // READ an entry
+  app.get('/entries/:id', function (req, res) {
+    const id = req.params.id;
+
+    // MongoDB requires an ID object
+    const details = { '_id': new ObjectID(id) };
+    myDB.collection('entries').findOne(details, function (err, item) {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
 };
 
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 // Initialize app as an instance of Express framework
 const app = express();
@@ -12,7 +13,12 @@ const port = 8000;
 // Use body-parser to process URL encoded forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
+// Connect the database
+MongoClient.connect(db.url, function (err, database) {
+  if (err) return console.log(err);
+  const myDB = database.db('myblog');
+  require('./app/routes')(app, myDB);
+})
 
 app.listen(port);
 
